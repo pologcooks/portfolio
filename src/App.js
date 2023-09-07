5dc3095d4977a12cc75a3dd97080319c5625264d
rdbz49ers <ralph.recinto1992@gmail.com>
2023-09-07 15:10:57 -0700

    Update about me section and add projects

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'; 
+import './App.css';
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Project from './components/Project';
@@ -9,14 +9,41 @@ function App() {
 
   return (
     <div className='app'>
-      <Header setCurrentSection={setCurrentSection}/>
+      <Header setCurrentSection={setCurrentSection} />
       <main className="content container my-4">
-        {currentSection === 'About Me' && <div className="bg-light p-4">About Me Content</div>}
+        {currentSection === 'About Me' &&
+          <div className="container my-5">
+            <h2>About Me</h2>
+            <div className="row">
+              <div className="col-lg-4">
+                <img src='/me.jpg' alt="Ralph Recinto" className="img-fluid rounded-circle mb-3" />
+              </div>
+              <div className="col-lg-8">
+                <p>
+                  Hello, my name is Ralph Receninto, I am a technology enthusiast who is eager to learn more about Software Development while utilizing
+                  my current experience and knowledge to help build something great. I have experience in full-stack development, including frontend, backend, and database management.
+                </p>
+                <p>
+                  In my free time, I like having family time and working on side projects.
+                </p>
+              </div>
+            </div>
+          </div>
+        }
         {currentSection === 'Portfolio' && (
           <div className="row">
-            <Project title="Project 1" link="#link1" repo="#repo1" />
-            <Project title="Project 2" link="#link2" repo="#repo2" />
-            {/* Add more Project components */}
+            <div className='col col-6'>
+              <Project title="SVG Logo Creator" link="https://pologcooks.github.io/Challenge_10/" repo="https://github.com/pologcooks/Challenge_10/" />
+            </div>
+            <div className='col col-6'>
+              <Project title="Readme Generator" link="https://pologcooks.github.io/Challenge_9/" repo="https://github.com/pologcooks/Challenge_9/" />
+            </div>
+            <div className='col col-6'>
+              <Project title="Ecommerce API" link="https://pologcooks.github.io/ecommerce-API/" repo="https://github.com/pologcooks/ecommerce-API" />
+            </div>
+            <div className='col col-6'>
+              <Project title="Share Square" link="https://sharesquare-c95734f95f40.herokuapp.com/" repo="https://github.com/DawsonBolen/project-3" />
+            </div>
           </div>
         )}
         {currentSection === 'Contact' && <div className="bg-light p-4">Contact Content</div>}
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Project = ({ title, link, repo }) => {
   return (
-    <div className="card m-2">
+    <div className="card m-1">
       <div className="card-body">
         <h3 className="card-title">{title}</h3>
-        <a className="btn btn-primary" href={link}>Live View</a>
-        <a className="btn btn-secondary" href={repo}>GitHub</a>
+        <a className="btn btn-primary m-1" href={link}>Live View</a>
+        <a className="btn btn-secondary m-1" href={repo}>GitHub</a>
       </div>
     </div>
   );
