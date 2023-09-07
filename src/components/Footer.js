bdd0eff0f039d5f2ba72e71b70da205aeab19df2
rdbz49ers <ralph.recinto1992@gmail.com>
2023-09-07 15:35:29 -0700

    Center footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-secondary text-white p-4">
+    <footer className="footer bg-secondary text-white p-4">
       <a className="text-white mx-2" href="https://github.com/pologcooks">GitHub</a>
       <a className="text-white mx-2" href="https://www.linkedin.com/in/ralph-recinto-a143b1107/">LinkedIn</a>
       <a className="text-white mx-2" href="https://twitter.com/Polotechh">Twitter</a>
